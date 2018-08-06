baa3db36ca2ba7f2ed7ee3394c31c44c9427e276
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-08-06 10:21:25 -0600

    app-5 complete

---
+++ app-5/src/Image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Image(props){
+    
+        return(
+            <div>
+
+                <img alt='Toby Mcguire meme' src={props.myurl}></img>
+
+            </div>
+        )
+    }
+
+
+
